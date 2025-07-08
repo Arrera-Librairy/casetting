34e94ea8d6e5605a189c35ed8704441c01dc57a3
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-07-08 22:08:10 +0200

    Mise a jour la librairy pour mettre le fichier de parametre dans un autre dossier que l'app

--- csetting.cpp
+++ csetting.cpp
@@ -1,12 +1,60 @@
-#include "casetting.h"
+#include "csetting.h"
 
 CSetting::CSetting() : fileOpen(false), fileCreated(false), settings(nullptr) {}
 
-CSetting::CSetting(const QString &file) {
+CSetting::CSetting(const QString &namesoft) {
+
+    int os = checkOS();
+    QString file;
+    QString standartFolder ;
+
+    if (os == 3 || os == 2){
+        QString standartFolder = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
+        if (QDir().mkdir(standartFolder+
+                         "/.config/"+namesoft))
+        {
+            return ;
+        }
+        file = standartFolder+
+               "/.config/"+namesoft+"/config.ini";
+
+    }else if (os == 1){
+        standartFolder =  QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+        if (QDir().mkdir(standartFolder)){
+            return ;
+        }
+        file = standartFolder+"\\config.ini";
+    }else {
+        file = namesoft+".ini";
+    }
+
+    //cout << QStandardPaths::writableLocation(QStandardPaths::AppDataLocation).toStdString() << endl;
+
     QFileInfo checkFile(file);
-    fileCreated = !checkFile.exists();
+
+    if (!checkFile.exists()) {
+        QFile newFile(file);
+        if (newFile.open(QIODevice::WriteOnly)) {
+            newFile.close();
+        }
+        fileCreated = true;
+    }
+    else{
+        fileCreated = false;
+    }
+
     settings = new QSettings(file, QSettings::IniFormat);
     fileOpen = true;
+}
+
+int CSetting::checkOS(){
+    #if defined(Q_OS_WIN)
+        return 1;
+    #elif defined(Q_OS_LINUX)
+        return 2;
+    #elif defined(Q_OS_MAC)
+        return 3;
+    #endif
 }
 
 bool CSetting::getFileCreated() const {
@@ -17,6 +65,7 @@ bool CSetting::setValeur(const QString &section, const QString &key, const QStri
     if (!fileOpen) return false;
     if (section.isEmpty() || key.isEmpty() || value.isEmpty()) return false;
     settings->setValue(section + "/" + key, value);
+    settings->sync();
     return true;
 }
 
--- csetting.h
+++ csetting.h
@@ -4,19 +4,31 @@
 #include <QSettings>
 #include <QString>
 #include <QFileInfo>
+#include <QStandardPaths>
+#include <QDir>
+
+// debut
+/*
+#include <iostream>
+using namespace std;
+*/
+
 
 class CSetting {
 private:
     bool fileOpen;
     bool fileCreated;
+    QString emplacementFile;
     QSettings *settings;
+    // Methode private
+    int checkOS();
 
 public:
     // Constructeur de base
     CSetting();
 
     // Constructeur avec le fichier
-    CSetting(const QString &file);
+    CSetting(const QString &namesoft);
 
     // Méthode pour obtenir l'état de création du fichier
     bool getFileCreated() const;
